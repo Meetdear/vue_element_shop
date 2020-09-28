018de229493a52446f94c4c791e6896166d6f504
Meetdear <meetdear@sina.com>
2020-09-28 14:15:02 +0800

    登录验证

--- src/main.js
+++ src/main.js
@@ -2,6 +2,22 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import {Button} from "element-ui"
+import {Form,FormItem} from "element-ui"
+import {Input} from "element-ui"
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+
+
+
+// 导入全局样式表
+import './assets/fonts/iconfont.css'
+import "./assets/css/global.css"
+
 
 Vue.config.productionTip = false
 
